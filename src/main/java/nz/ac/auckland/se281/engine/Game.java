8ba9e8c1ebfc3092f3450c93960403018e2482b6
harrywu23 <hwu756@aucklanduni.ac.nz>
2025-05-12 15:31:58 +1200

    added logic so that it asks for a user input after saying what round it is. Also made the program print invalid input if the input was invalid.

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,12 +2,14 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
 
   public static String AI_NAME = "HAL-9000";
   private int numRounds;
   private int currentRound = 1;
+  private String input;
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
     this.numRounds = numRounds;
@@ -16,11 +18,18 @@ public class Game {
       MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
     }
   }  
-  
+
   public void play() {
     if (currentRound <= numRounds){
       MessageCli.START_ROUND.printMessage(String.valueOf(currentRound), String.valueOf(numRounds));
       currentRound++; 
+    }
+    MessageCli.ASK_HUMAN_INPUT.printMessage();
+    Colour userInput = Colour.fromInput(input);
+    for (Colour c : Colour.values()) {
+      if (c != userInput){
+        MessageCli.INVALID_HUMAN_INPUT.printMessage();
+      }
     }
   }
 
