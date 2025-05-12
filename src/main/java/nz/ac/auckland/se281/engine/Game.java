ca7559c9aeea5cae16d1d6354b7379cad16486f6
harrywu23 <hwu756@aucklanduni.ac.nz>
2025-05-12 15:07:56 +1200

    added logic to pass test case 2.

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -6,15 +6,22 @@ import nz.ac.auckland.se281.cli.MessageCli;
 public class Game {
   public static String AI_NAME = "HAL-9000";
 
-  public Game() {}
+  private int numRounds;
+  private int currentRound;
+  public Game() {
+  }
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    this.numRounds = numRounds;
+    this.currentRound = 1;
     if (options.length > 0) {
       String namePlayer = options[0];
       MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
     }
   }  
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(currentRound), String.valueOf(numRounds)); 
+  }
 
   public void showStats() {}
 }
