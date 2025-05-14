b3873edfb4c4b659a340bb78cc86de52fbc0e967
harrywu23 <hwu756@aucklanduni.ac.nz>
2025-05-14 18:20:24 +1200

    fixed the factory taking a string instead of an enum

--- src/main/java/nz/ac/auckland/se281/model/Difficulty.java
+++ src/main/java/nz/ac/auckland/se281/model/Difficulty.java
@@ -1,7 +1,5 @@
 package nz.ac.auckland.se281.model;
 
-import nz.ac.auckland.se281.engine.Game;
-
 public interface Difficulty {
-    public AIStrategy createStrategy(Game game);
+  public AIStrategy getStrategy();
 }
--- src/main/java/nz/ac/auckland/se281/model/Medium.java
+++ src/main/java/nz/ac/auckland/se281/model/Medium.java
@@ -1,11 +1,9 @@
 package nz.ac.auckland.se281.model;
 
-import nz.ac.auckland.se281.engine.Game;
-
 public class Medium implements Difficulty {
 
   @Override
-  public AIStrategy createStrategy(Game game) {
+  public AIStrategy getStrategy() {
     return new AvoidLastStrategy();
   }
 }
