456aadbbfd872f798d8df7900ee4fe5e4cfd449f
Harry Wu <hwu756@aucklanduni.ac.nz>
2025-05-17 20:26:09 +1200

    passed test case 2, added and fixed hard logic.

--- src/main/java/nz/ac/auckland/se281/model/LeastUsedStrategy.java
+++ src/main/java/nz/ac/auckland/se281/model/LeastUsedStrategy.java
@@ -20,28 +20,42 @@ public class LeastUsedStrategy implements AIStrategy {
   @Override
   public Colour getAiGuess(Colour lastChosenColour) {
     ArrayList<Colour> history = game.getHistoryOfColours();
-    ArrayList<Colour> alreadyCountedColours = new ArrayList<>();
-    int minCount = Integer.MAX_VALUE;
+    int redCount = 0;
+    int greenCount = 0;
+    int blueCount = 0;
+    int yellowCount = 0;
 
     // checking which colour has been used the least
-    for (Colour colour : history) {
-      if (alreadyCountedColours.contains(colour)) {
-        // skip because we have already counted this colour
+    for (Colour currentColour : history) {
+      if (currentColour == null) {
         continue;
       }
 
-      int count = 0;
-      for (Colour currentColour : history) {
-        if (currentColour != null && currentColour.equals(colour)) {
-          count++;
-        }
+      if (currentColour.equals(Colour.RED)) {
+        redCount++;
+      } else if (currentColour.equals(Colour.GREEN)) {
+        greenCount++;
+      } else if (currentColour.equals(Colour.BLUE)) {
+        blueCount++;
+      } else if (currentColour.equals(Colour.YELLOW)) {
+        yellowCount++;
       }
+    }
 
-      if (count < minCount) {
-        minCount = count;
-        leastUsed = colour;
-      }
-      alreadyCountedColours.add(colour); // Mark it as counted AFTER counting
+    Colour leastUsed = Colour.RED;
+    int minCount = redCount;
+
+    if (greenCount < minCount) {
+      leastUsed = Colour.GREEN;
+      minCount = greenCount;
+    }
+    if (blueCount < minCount) {
+      leastUsed = Colour.BLUE;
+      minCount = blueCount;
+    }
+    if (yellowCount < minCount) {
+      leastUsed = Colour.YELLOW;
+      minCount = yellowCount;
     }
     return leastUsed;
   }
